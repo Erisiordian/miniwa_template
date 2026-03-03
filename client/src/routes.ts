a937d9d02d7afccc46fbcdc0321c2d68ed6d2ae5
Erisiordian <hat.luka10@gmail.com>
2026-03-03 14:37:04 +0100

    chore(client): consolidate routes and use authGuard

--- client/src/routes.ts
+++ client/src/routes.ts
@@ -4,7 +4,7 @@ import { authGuard } from './app/core/auth.guard';
 export const routes: Routes = [
   { path: '', redirectTo: 'query', pathMatch: 'full' },
   { path: 'query', loadComponent: () => import('./app/pages/query/query.page').then(m => m.QueryPage) },
-  { path: 'history', loadComponent: () => import('./app/pages/history/history.page').then(m => m.HistoryPage) },
+  { path: 'history', canActivate: [authGuard], loadComponent: () => import('./app/pages/history/history.page').then(m => m.HistoryPage) },
   { path: 'notebooks', canActivate: [authGuard], loadComponent: () => import('./app/pages/notebooks/notebooks.page').then(m => m.NotebooksPage) },
   { path: 'login', loadComponent: () => import('./app/pages/auth/login.page').then(m => m.LoginPage) },
   { path: 'register', loadComponent: () => import('./app/pages/auth/register.page').then(m => m.RegisterPage) },
